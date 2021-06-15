ef94bac7d3b1775dcbd992e1a839f8b243c38561
Joel Purra <code+github@joelpurra.com>
2021-06-15 10:17:05 +0200

    Upgrade dependencies (#9)
    * Add .npmrc to disable package-lock.json
    
    - The file `package-lock.json` is not that useful for libraries, since it is not included in the published `npm` package.
    - Using `.gitignore` to not commit `package-lock.json` kind of works, but disabling it in `.npmrc` seems better.
    - Clones of the repository from before this change may need to manually delete `package-lock.json` locally.
    
    * Update dependencies in-range
    
    * Replace deprecated request-promise with got
    
    - The package `request-promise` was deprecated on 2020-02-11.
    - Also fixes security audit warnings when installing `chrome-webstore-manager` using `npm`.
    
    ```text
    request-promise  0.2.4 - 2.0.0
    Depends on vulnerable versions of lodash
    node_modules/request-promise
      chrome-webstore-manager  *
      Depends on vulnerable versions of request-promise
      node_modules/chrome-webstore-manager
    ```
    
    See
    
    - https://github.com/request/request-promise#deprecated
    - https://github.com/request/request/issues/3142
    - https://github.com/sindresorhus/got/blob/main/documentation/migration-guides.md
    
    * Replace opn with renamed package open
    
    - The package has been renamed from `opn` to `open`.
    
    See
    
    - https://github.com/sindresorhus/open
    
    * Upgrade standard
    
    * Run standard --fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
-var readline = require('readline')
-var fs = require('fs')
-var program = require('commander')
-var open = require('opn')
-var getToken = require('./libs/getToken')
-var ChromeWebstore = require('./libs/chrome-webstore.js')
+const readline = require('readline')
+const fs = require('fs')
+const program = require('commander')
+const open = require('open')
+const getToken = require('./libs/getToken')
+const ChromeWebstore = require('./libs/chrome-webstore.js')
 
-var getAccessToken = function (cid, cs, code) {
+const getAccessToken = function (cid, cs, code) {
   const chromeWebstore = new ChromeWebstore(cid, cs)
   chromeWebstore.getAccessToken(code)
-    .then(function (data) {
-      var json = JSON.parse(data)
+    .then(function (json) {
       console.log('Your token: ' + json.access_token)
       console.log('Your refresh_token: ' + json.refresh_token)
       process.exit()
@@ -28,7 +27,7 @@ program
   .option('--client_secret [Client_Secret]', 'Your Client Secret')
   .option('--code [CODE]', 'Your authorized code')
   .action(function (options) {
-    var rli = readline.createInterface(process.stdin, process.stdout)
+    const rli = readline.createInterface(process.stdin, process.stdout)
     const cid = options.client_id
     const cs = options.client_secret
     if (!(cid && cs)) {
@@ -53,11 +52,10 @@ program
   .description('create new item on Chrome Web Store')
   .option('-t, --token [YOUR_TOKEN]', 'Your token')
   .action(function (zipFile, options) {
-    var token = getToken(options)
-    var fileBin = fs.readFileSync(zipFile)
+    const token = getToken(options)
+    const fileBin = fs.readFileSync(zipFile)
     const chromeWebstore = new ChromeWebstore()
-    chromeWebstore.insertItem(token, fileBin).then(function (data) {
-      var json = JSON.parse(data)
+    chromeWebstore.insertItem(token, fileBin).then(function (json) {
       if (json.itemError) {
         console.error(json.itemError)
         return process.exit(1)
@@ -71,11 +69,10 @@ program
   .description('update your item')
   .option('-t, --token [YOUR_TOKEN]', 'Your token')
   .action(function (itemId, zipFile, options) {
-    var fileBin = fs.readFileSync(zipFile)
-    var token = getToken(options)
+    const fileBin = fs.readFileSync(zipFile)
+    const token = getToken(options)
     const chromeWebstore = new ChromeWebstore()
-    chromeWebstore.updateItem(token, fileBin, itemId).then(function (data) {
-      var json = JSON.parse(data)
+    chromeWebstore.updateItem(token, fileBin, itemId).then(function (json) {
       if (json.itemError) {
         console.error(json.itemError)
         return process.exit(1)
@@ -91,10 +88,9 @@ program
   .option('--projection [PROJECTION_TYPE]', '"DRAFT" or "PUBLISHED"')
   .action(function (itemId, options) {
     const token = getToken(options)
-    var projection = options.projection || 'DRAFT'
+    const projection = options.projection || 'DRAFT'
     const chromeWebstore = new ChromeWebstore()
-    chromeWebstore.getItem(token, itemId, projection).then(function (data) {
-      var json = JSON.parse(data)
+    chromeWebstore.getItem(token, itemId, projection).then(function (json) {
       if (json.itemError) {
         console.error(json.itemError)
         return process.exit(1)
@@ -109,11 +105,10 @@ program
   .option('-t, --token [YOUR_TOKEN]', 'Your token')
   .option('--target [TARGET_TYPE]', '"trustedTesters" or "default"')
   .action(function (itemId, options) {
-    var token = getToken(options)
-    var target = options.target || 'default'
+    const token = getToken(options)
+    const target = options.target || 'default'
     const chromeWebstore = new ChromeWebstore()
-    chromeWebstore.publishItem(token, itemId, target).then(function (data) {
-      var json = JSON.parse(data)
+    chromeWebstore.publishItem(token, itemId, target).then(function (json) {
       if (json.itemError) {
         console.error(json.itemError)
         return process.exit(1)
@@ -138,10 +133,9 @@ program
       console.error('Require refresh_token')
       process.exit(1)
     }
-    var refreshToken = options.refresh_token || process.env.WEBSTORE_REFRESH_TOKEN
+    const refreshToken = options.refresh_token || process.env.WEBSTORE_REFRESH_TOKEN
     const chromeWebstore = new ChromeWebstore(cid, cs)
-    chromeWebstore.getRefreshToken(refreshToken).then(function (data) {
-      var json = JSON.parse(data)
+    chromeWebstore.getRefreshToken(refreshToken).then(function (json) {
       console.log(json.access_token)
     })
   })
--- src/libs/chrome-webstore.js
+++ src/libs/chrome-webstore.js
@@ -1,71 +1,82 @@
-const request = require('request-promise')
+const got = require('got')
 module.exports = class ChromeWebStore {
   constructor (cid, cs, redirectUrl) {
     this.cid = cid
     this.cs = cs
     this.redirectUrl = redirectUrl || 'urn:ietf:wg:oauth:2.0:oob'
   }
+
   getCodeUrl (redirectUrl, state) {
     state = state || ''
     state = encodeURIComponent(state)
     return 'https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=' + this.cid + '&state' + state + '&redirect_uri=' + (redirectUrl || this.redirectUrl)
   }
+
   getAccessToken (code, redirectUrl) {
-    return request.post('https://accounts.google.com/o/oauth2/token')
-      .form({
-        client_id: this.cid,
-        client_secret: this.cs,
-        code: code,
-        grant_type: 'authorization_code',
-        redirect_uri: redirectUrl || this.redirectUrl
+    return got.post('https://accounts.google.com/o/oauth2/token',
+      {
+        form: {
+          client_id: this.cid,
+          client_secret: this.cs,
+          code: code,
+          grant_type: 'authorization_code',
+          redirect_uri: redirectUrl || this.redirectUrl
+        }
+      }).json()
+  }
+
+  insertItem (token, fileBin) {
+    return got.post('https://www.googleapis.com/upload/chromewebstore/v1.1/items',
+      {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'x-goog-api-version': 2
+        },
+        body: fileBin
+      }).json()
+  }
+
+  getItem (token, itemId, projection = 'DRAFT') {
+    return got.get('https://www.googleapis.com//chromewebstore/v1.1/items/' + itemId + '?projection=' + projection,
+      {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'x-goog-api-version': 2
+        }
+      }).json()
+  }
+
+  updateItem (token, fileBin, itemId) {
+    return got.put('https://www.googleapis.com/upload/chromewebstore/v1.1/items/' + itemId,
+      {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'x-goog-api-version': 2
+        },
+        body: fileBin,
+        timeout: 120 * 1000
+      }).json()
+  }
+
+  publishItem (token, itemId, target = 'default') {
+    return got.post('https://www.googleapis.com//chromewebstore/v1.1/items/' + itemId + '/publish?publishTarget=' + target,
+      {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'x-goog-api-version': 2
+        }
       })
   }
-  insertItem (token, fileBin) {
-    return request.post({
-      uri: 'https://www.googleapis.com/upload/chromewebstore/v1.1/items',
-      headers: {
-        Authorization: 'Bearer ' + token,
-        'x-goog-api-version': 2
-      },
-      body: fileBin
-    })
-  }
-  getItem (token, itemId, projection = 'DRAFT') {
-    return request.get({
-      uri: 'https://www.googleapis.com//chromewebstore/v1.1/items/' + itemId + '?projection=' + projection,
-      headers: {
-        Authorization: 'Bearer ' + token,
-        'x-goog-api-version': 2
-      }
-    })
-  }
-  updateItem (token, fileBin, itemId) {
-    return request({
-      method: 'PUT',
-      uri: 'https://www.googleapis.com/upload/chromewebstore/v1.1/items/' + itemId,
-      headers: {
-        Authorization: 'Bearer ' + token,
-        'x-goog-api-version': 2
-      },
-      body: fileBin,
-      timeout: 120 * 1000
-    })
-  }
-  publishItem (token, itemId, target = 'default') {
-    return request.post({
-      uri: 'https://www.googleapis.com//chromewebstore/v1.1/items/' + itemId + '/publish?publishTarget=' + target,
-      headers: {
-        Authorization: 'Bearer ' + token,
-        'x-goog-api-version': 2
-      }
-    })
-  }
+
   getRefreshToken (refreshToken) {
-    return request.post('https://www.googleapis.com/oauth2/v3/token', { form: {
-      client_id: this.cid,
-      client_secret: this.cs,
-      grant_type: 'refresh_token',
-      refresh_token: refreshToken
-    } })
+    return got.post('https://www.googleapis.com/oauth2/v3/token',
+      {
+        form: {
+          client_id: this.cid,
+          client_secret: this.cs,
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken
+        }
+      }).json()
   }
 }
